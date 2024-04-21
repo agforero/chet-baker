02a4513c856e178c1f531dce1fec948ffd1dd6b2
Agustin Forero <agforero@gmail.com>
2024-04-20 21:04:04 -0400

    new sections

--- src/app/data/reportText.ts
+++ src/app/data/reportText.ts
@@ -15,6 +15,18 @@ _a project by Maximos Forero_`,
 				text: "Chet Baker was a founding pioneer of Cool Jazz due to his trumpeting and vocal skills, which helped spread jazz worldwide and make people feel comfortable. His artistic expression touched many and influenced more. In the words of Ruth Young, a singer, “It would be like living with Picasso.” (Weber) Today, people who recognize Chet Baker's name will know him as a figure well known for his improvisation, which impacted the jazz he had started and how it would evolve.",
 				image: "macnamara.jpg",
 			},
+			{
+				text: "Chesney Henry Baker Jr. was born to a pianist and a guitarist on December 23rd, 1929; Chet Baker had to grow up in the great depression when his mother and father had to give up their musical careers and work factory jobs or grab any job that they could have. First, he lived in Oklahoma for ten years, then after moving to California, Chet Baker was given a trombone, which proved far too large for him at his age. One day, his father bought an old trumpet from a pawn shop, which wasn't for Chet, but he used it anyway. Over the next two weeks of getting a hold of this trumpet, Chet showed an astounding level of mastery; he would listen to the radio, and somehow, without reading off the music or knowing precisely what each valve press meant on the trumpet, he would match the notes he would hear with the trumpet and perfectly replicate the song. (Valk)",
+				image: "claxton.jpg",
+			},
+			{
+				text: `Over thirty years, Chet Baker would make an impressive number of albums, almost 170. This all began with his most trusted associate, Gerry Mulligan, a saxophonist. Chet Baker joined his quartet in 1952 and made waves due to his natural fantastic ability. His first recorded song, "My Funny Valentine," would be a monumental moment in his career. Proceeding this in 1954, Chet Baker would record another significant album, "Chet Baker Sings". After about a year of jumping between recording studios and clubs, Chet Baker made his first European tour to record in France and to make his name well known. After returning to the United States, he would record with Gerry Mulligan and other well-known cats, such as Bill Evans and Paul Chambers, who ironically were associated with Chet Baker's opposing trumpeter, Miles Davis. (Gavin)`,
+				image: "claxton-2.jpg",
+			},
+			{
+				text: "Although Having to deal with drug problems almost all his life, things truly started to turn for the worse following 1957, which is when Chet Baker began to do heroin. Anywhere between this time and 1973, Chet Baker was on a severe financial decline and was in and out of jail. He would sell his belongings just to buy more drugs and support what little family connections he had left, but nothing was heavier on Chet Baker as an artist than his incident in 1966. (Gavin) Even though he changed his story from person to person, Chet Baker, while trying to get heroin from a former associate of his, had been assaulted by a group of young men, which led him to lose all of his teeth (except one). This heavily impacted his ability to play brass. Following this, Chet Baker tried to make ends meet and slowed down his performances; in 1987, he made a documentary called “Let’s Get Lost” with Bruce Weber about his own life, and while in the final stages of production, Chet Baker had his final accident. On May 13th, 1988, Chet Baker was found outside of a hotel in Amsterdam where he had fallen out of his hotel room because he had slept on the window sill; he was found with heroin and cocaine in his bloodstream.(Weber)",
+				image: "guardian.png",
+			},
 		],
 	},
 ];
