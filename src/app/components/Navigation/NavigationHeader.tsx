cce4e81712cc61d2b557b48dbbf91d22907a12c1
Agustin Forero <agforero@gmail.com>
2024-04-21 20:09:51 -0400

    yet anotha bug squashed

--- src/app/components/Navigation/NavigationHeader.tsx
+++ src/app/components/Navigation/NavigationHeader.tsx
@@ -13,13 +13,7 @@ export default function NavigationHeader() {
 	return (
 		<Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "100%" }}>
 			<MusicPlayer />
-			<NavigationButton
-				text="About"
-				onPress={() => router.push("/tutorial")}
-				tooltipContent={"Open the tutorial, or see more information about this page."}
-				tooltipPlacement="bottom"
-				endIcon={<HelpOutlineOutlined />}
-			/>
+			<NavigationButton text="About" onPress={() => router.push("/tutorial")} endIcon={<HelpOutlineOutlined />} />
 		</Box>
 	);
 }
